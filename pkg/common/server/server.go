5dc4233e85f3af8899e54bd53cac476e35d89d82
Héricles <hericles.emanuel@nuveo.ai>
2021-08-21 14:30:54 -0700

    chg: deps: update echo framework to v4

--- pkg/common/server/server.go
+++ pkg/common/server/server.go
@@ -8,8 +8,8 @@ import (
 	"time"
 
 	"github.com/go-playground/validator"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // New instantates new Echo server
