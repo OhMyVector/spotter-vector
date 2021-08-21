15e695a1e6d37260ad0aeab76c91d8948d94b514
Héricles Emanuel <me@piccklee.dev>
2021-08-21 17:27:28 -0300

    Feat: API Structure (#10)
    * new: feat: create cmd folder and scripts
    
    * new: feat: create config parser
    
    * new: feat: create new base server using echo
    
    * chg: chore: move static files to public folder
    
    * chg: feat: recreate api package
    
    * new: chore: create dockerignore
    
    * chg: refactor: update dockerfile
    
    * chg: refactor: update healthcheck endpoint

---
+++ pkg/api/api.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/ohmyvector/spotter-vector/pkg/api/websocket"
+	"github.com/ohmyvector/spotter-vector/pkg/common/config"
+	"github.com/ohmyvector/spotter-vector/pkg/common/server"
+)
+
+func Start(cfg *config.Configuration) error {
+
+	e := server.New()
+
+	// Register static assets
+	e.Static("/", cfg.App.StaticPath)
+
+	// Register websocket handler
+	e.GET("/ws", websocket.Connection)
+
+	server.Start(e, &server.Config{
+		Port:                cfg.Server.Port,
+		ReadTimeoutSeconds:  cfg.Server.ReadTimeout,
+		WriteTimeoutSeconds: cfg.Server.WriteTimeout,
+		Debug:               cfg.Server.Debug,
+	})
+
+	return nil
+}
---
+++ pkg/api/websocket/websocket.go
@@ -0,0 +1,56 @@
+package websocket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func reader(c echo.Context, conn *websocket.Conn) error {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+			return err
+		}
+		c.Logger().Info(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			c.Logger().Error(err)
+			return err
+		}
+	}
+}
+
+func Connection(c echo.Context) (err error) {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		c.Logger().Error(err)
+		return
+	}
+	defer ws.Close()
+
+	c.Logger().Info("Client Connected")
+
+	err = ws.WriteMessage(1, []byte("Hi Spotter!"))
+	if err != nil {
+		c.Logger().Error(err)
+		return
+	}
+
+	err = reader(c, ws)
+	if err != nil {
+		c.Logger().Error(err)
+		return
+	}
+	return
+}
