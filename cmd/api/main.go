c64fe12daeb9e797d0a1db5ddbb714dc2b8931a0
Héricles Emanuel <me@piccklee.dev>
2021-08-21 17:27:28 -0300

    Feat: API Structure (#10)
    * new: feat: create cmd folder and scripts
    
    * new: feat: create config parser
    
    * new: feat: create new base server using echo
    
    * chg: chore: move static files to public folder
    
    * chg: feat: recreate api package
    
    * new: chore: create dockerignore
    
    * chg: refactor: update dockerfile
    
    * chg: refactor: update healthcheck endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/ohmyvector/spotter-vector/pkg/api"
+	"github.com/ohmyvector/spotter-vector/pkg/common/config"
+)
+
+func main() {
+
+	cfgPath := flag.String("p", "conf.local.yaml", "Path to config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*cfgPath)
+	checkErr(err)
+
+	checkErr(api.Start(cfg))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
