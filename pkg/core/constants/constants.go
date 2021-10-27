0e5ae54ee349487a293d80f38e95d62a7bb63f06
Héricles <hericles.emanuel@nuveo.ai>
2021-10-27 12:39:27 -0300

    new: feature: create constants package

---
+++ pkg/core/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+import "math"
+
+const VECTOR_ID_TAG = 2000001
+
+/* Movement Settings */
+const SPEED_MMPS = 100
+const BASE_DISTANCE = 10
+const TURN_LEFT_ANGLE_RAD = (5 * math.Pi / 180)
+const TURN_RIGHT_ANGLE_RAD = (-5 * math.Pi / 180)
